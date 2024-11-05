9be73b4ed2889758071d37c33223a0382b0e43c8
Carlos Raez <carlosraez@gmail.com>
2024-11-05 20:06:13 +0100

    feat navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { Clock, HomeIcon, Search, Package2 } from 'lucide-react';
+import { Clock, HomeIcon, Search, Package2, ArrowLeftRight } from 'lucide-react';
 
 const Navbar = () => {
   return (
@@ -15,49 +15,50 @@ const Navbar = () => {
           </Link>
           
           <div className="flex space-x-6">
-            <Link 
-              href="/" 
+            <Link
+              href="/"
               className="flex items-center space-x-1 text-gray-600 hover:text-gray-900"
             >
               <HomeIcon className="w-4 h-4" />
               <span>Inicio</span>
             </Link>
             
-            <Link 
-              href="/ayuda/necesito" 
+            <Link
+              href="/ayuda/necesito"
               className="flex items-center space-x-1 text-red-600 hover:text-red-700"
             >
               <span>Necesito Ayuda</span>
             </Link>
             
-            <Link 
-              href="/ayuda/ofrezco" 
+            <Link
+              href="/ayuda/ofrezco"
               className="flex items-center space-x-1 text-green-600 hover:text-green-700"
             >
               <span>Ofrezco Ayuda</span>
             </Link>
+
+            <Link
+              href="/intercambio"
+              className="flex items-center space-x-1 text-purple-600 hover:text-purple-700"
+            >
+              <ArrowLeftRight className="w-4 h-4" />
+              <span>Intercambio</span>
+            </Link>
             
-            <Link 
-              href="/recursos" 
+            <Link
+              href="/recursos"
               className="flex items-center space-x-1 text-orange-600 hover:text-orange-700"
             >
               <Package2 className="w-4 h-4" />
               <span>Recursos</span>
             </Link>
             
-            <Link 
-              href="/listado" 
+            <Link
+              href="/listado"
               className="flex items-center space-x-1 text-blue-600 hover:text-blue-700"
             >
               <Search className="w-4 h-4" />
               <span>Ver Listado</span>
-            </Link>
-            <Link 
-              href="/intercambio" 
-              className="flex items-center space-x-1 text-orange-600 hover:text-orange-700"
-            >
-              <Package2 className="w-4 h-4" />
-              <span>Intercambio Pueblos</span>
             </Link>
           </div>
         </div>
